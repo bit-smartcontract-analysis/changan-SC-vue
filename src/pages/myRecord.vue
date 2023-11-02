397404d84dbced4a03d2121d8a514e4b10b1f592
WeiZ-boot <74607045+WeiZ-boot@users.noreply.github.com>
2023-11-02 14:36:47 +0800

    add check icons

--- src/pages/myRecord.vue
+++ src/pages/myRecord.vue
@@ -1,57 +1,70 @@
 <template>
-
-<div class="container">
-  <div class="file-list" v-for="file in contract_list" :key="file.name">
-    <div class="file-info">
-      <span class="file-name">{{ file.name }}</span>
-      <span class="file-size">{{ file.size }} bytes</span>
-    </div>
-    <div class="file-actions">
-      <el-button type="danger" circle @click="deleteContract(file.name)"><el-icon><delete /></el-icon> </el-button>
+  <div class="container">
+    <div class="file-list" v-for="file in contract_list" :key="file.name">
+      <div class="file-info">
+        <span class="file-name">{{ file.name }}</span>
+        <span class="file-size">{{ file.size }} bytes</span>
+      </div>
+      <div class="file-actions">
+        <el-button type="success" circle @click="checkContract(file.name)">
+          <el-icon><edit /></el-icon>
+        </el-button>
+        <el-button type="danger" circle @click="deleteContract(file.name)"
+          ><el-icon><delete /></el-icon>
+        </el-button>
+      </div>
     </div>
   </div>
-</div>
-
 </template>
 
-<script>
-import { Delete } from "@element-plus/icons";
-export default {
-    name: "AppContractManage",
-    components: {
-        Delete,
-    },
-    data() {
-        return {
-            contract_list: [],
-        }
-    },
-    mounted() {
-        this.fetchContractList(); 
-    },
-    methods: {
-        fetchContractList() {
-            this.$http.getLogsList().then((response) => {
-                this.contract_list = response.files; // Extract the 'files' array from the response
-            }).catch((error) => {
-                console.error("Failed to fetch contract list:", error);
-            });
-        },
-        deleteContract(contract) {
-            console.log(contract);
-            this.$http.getContractDelete(contract).then(response => {
-                console.log(response);
-                // After successful deletion, fetch the updated list
-                this.fetchContractList();
-            })
-            .catch(error => {
-                // Handle the error here
-                console.error('Failed to delete contract:', error);
-            });
-        }
-    }
-}
 
+<script>
+import { Delete,  Edit} from "@element-plus/icons";
+export default {
+  name: "AppContractManage",
+  components: {
+    Delete,
+    Edit
+  },
+  data() {
+    return {
+      contract_list: [],
+    };
+  },
+  mounted() {
+    this.fetchContractList();
+  },
+  methods: {
+    fetchContractList() {
+      this.$http
+        .getLogsList()
+        .then((response) => {
+          this.contract_list = response.files; // Extract the 'files' array from the response
+        })
+        .catch((error) => {
+          console.error("Failed to fetch contract list:", error);
+        });
+    },
+    deleteContract(contract) {
+      console.log(contract);
+      this.$http
+        .getContractDelete(contract)
+        .then((response) => {
+          console.log(response);
+          // After successful deletion, fetch the updated list
+          this.fetchContractList();
+        })
+        .catch((error) => {
+          // Handle the error here
+          console.error("Failed to delete contract:", error);
+        });
+    },
+    checkContract(fileName) {
+      // Your check logic here
+      console.log(`Checked contract: ${fileName}`);
+    },
+  },
+};
 </script>
 
 <style scoped>

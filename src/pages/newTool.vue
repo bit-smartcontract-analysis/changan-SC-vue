29d49d099b1084f109729fbf8e1d742fdf9022d1
yangxiaoxuan123 <1145309483@qq.com>
2024-02-27 19:05:11 +0800

    new

--- src/pages/newTool.vue
+++ src/pages/newTool.vue
@@ -1,108 +1,102 @@
 <template>
-    <p class="line">轻触卡片开始你的合约检测吧！</p>
-    <div class="container">
-        <!-- 第一个卡片 -->
-        <div class="card1">
-            <div class="content">
-                <h2>01</h2>
-                <h3>01 上传合约</h3>
-                <el-input v-model="contract_url" autocomplete="off" style="margin-right: 10px"></el-input>
-                <br>
-                <br>
-                <el-upload
-                    :file-list="fileList"
-                    class="upload-demo"
-                    :action="$http.server_host + '/toolFunc/contracts/upload'"
-                    :headers="{ Authorization: 'Bearer ' + $auth.token }"
-                    multiple
-                    :on-preview="handlePreview"
-                    :on-remove="handleRemove"
-                    :before-remove="beforeRemove"
-                    :limit="3"
-                    :on-exceed="handleExceed"
-                    :on-success="handleSuccess"
-                    :on-error="handleError"
-                >
-                
-                <el-button class="mybutton" type="primary" >上传合约<el-icon class="el-icon--right"><Upload /></el-icon></el-button>
-                <div class="el-upload__tip">
-                    contract files with a size less than 500KB.
-                </div>
-                <br>
-                </el-upload>
-                <div class="aaaaa">
-                <div class="file-list-container">
-                    <div class="file" v-for="file in paginatedFiles" :key="file.name" style="color:#696969">
-                        {{ file }}
-                    </div>
-                </div>
-                </div>
-             
-                
-            </div>
-        </div>
-        <!-- 第二个卡片 -->
-        <div class="card1">
-            <div class="content">
-                <h2>02</h2>
-                <h3>02 工具选择</h3>
-                <br>
-                <div class="select-button-container">
-                    <el-select v-model="selectedOption" :options="options" placeholder="工具">
-                    <el-option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value"
-                    />
-                    </el-select>
-                    <br>
-                    <el-select v-model="contract_name" :options="options" placeholder="合约">
-                   
-                    <el-option
-                        v-for="item in contract_list"
-                        :key="item"
-                        :label="item"
-                        :value="item"
-                    />
-                    </el-select>
-                    <br>
-                    <el-button type="success" @click="handleButtonClick"
-                    >开始检测<el-icon><Share /></el-icon
-                    ></el-button>
-                </div>
-            </div>
-        </div>
-        <!-- 第三个卡片 -->
-        <div class="card2">
-            <div class="content">
-                <h2>03</h2>
-                <h3>03 检测结果</h3>
-                <br>
-                <div class="description-container">
-                    <el-descriptions
-                        class="margin-top"
-                        :column="3"
-                        :size="size"
-                        :contentStyle="rowCenter" 
-                        :labelStyle="rowCenter"
-                        border
-                    >
-                    <el-descriptions-item label="工具名称" width="10%">
-                        {{ selectedOption }}
-                    </el-descriptions-item>
-                    <el-descriptions-item label="是否成功" width="10%">
-                        {{ message }}</el-descriptions-item
-                    >
-                    <el-descriptions-item label="所用时间" width="10%">
-                        {{ execution_time }}
-                    </el-descriptions-item>
-                    </el-descriptions>
-                        <result-component :detect_res="detect_res"></result-component>
-                    </div>
-            </div>
-        </div>
+  <br>
+  <div class="container">
+    <div class="top">合约检测</div>
+       <ol class="rounded-list">
+        <li><a href="">上传合约</a></li>      
+    <!-- 第一个卡片 -->
+    <div class="card1 card">
+      <div class="content">
+         <el-input
+          v-model="contract_url"
+          autocomplete="off"
+          style="margin-right: 10px"></el-input>
+        <el-upload
+          :file-list="fileList"
+          class="upload-demo"
+          :action="$http.server_host + '/toolFunc/contracts/upload'"
+          :headers="{ Authorization: 'Bearer ' + $auth.token }"
+          multiple
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+          :limit="3"
+          :on-exceed="handleExceed"
+          :on-success="handleSuccess"
+          :on-error="handleError">
+          <el-button class="mybutton1" type="primary"
+            >上传合约<el-icon class="el-icon--right"><Upload /></el-icon
+          ></el-button>
+          <div class="el-upload__tip">
+            contract files with a size less than 500KB.
+          </div>
+          <br />
+        </el-upload>
+      </div>
     </div>
+    <hr>
+    <br>
+        <li><a href="">选择工具</a></li>
+    <!-- 第二个卡片 -->
+    <div class="card1 card">
+      <div class="content">
+        <el-select
+          v-model="selectedOption"
+          :options="options"
+          placeholder="工具">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value" />
+        </el-select>
+        <el-select
+          v-model="contract_name"
+          :options="options"
+          placeholder="合约">
+          <el-option
+            v-for="item in contract_list"
+            :key="item"
+            :label="item"
+            :value="item" />
+        </el-select>
+        <el-button class="mybutton2" type="success" @click="handleButtonClick"
+          >开始检测<el-icon><Share /></el-icon
+        ></el-button>
+      </div>
+    </div>
+    <br>
+    <hr>
+    <br>
+    <li><a href="">检测结果</a></li>
+    </ol>
+   
+    <!-- 第三个卡片 -->
+    <div class="card2 card">
+      <div class="content">
+        <div class="description-container">
+          <el-descriptions
+            class="margin-top"
+            :column="3"
+            :size="size"
+            :contentStyle="rowCenter"
+            :labelStyle="rowCenter"
+            border>
+            <el-descriptions-item label="工具名称" width="10%">
+              {{ selectedOption }}
+            </el-descriptions-item>
+            <el-descriptions-item label="是否成功" width="10%">
+              {{ message }}</el-descriptions-item
+            >
+            <el-descriptions-item label="所用时间" width="10%">
+              {{ execution_time }}
+            </el-descriptions-item>
+          </el-descriptions>
+          <result-component :detect_res="detect_res"></result-component>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { ElMessage, ElMessageBox } from "element-plus";
@@ -181,9 +175,9 @@ export default {
       execution_time: "",
     };
   },
-  rowCenter:{
-                    "text-align":"center"
-        },
+  rowCenter: {
+    "text-align": "center",
+  },
   computed: {
     paginatedFiles() {
       const start = (this.currentPage - 1) * 5;
@@ -288,20 +282,82 @@ export default {
 };
 </script>
 
-
-
 <style scoped>
+ol{
+        counter-reset: li; /* 创建一个计数器 */
+        list-style: true; /* 清除列表默认的编码*/
+        *list-style: decimal; /* 让IE6/7具有默认的编码 */
+        font: 15px 'trebuchet MS', 'lucida sans';
+        padding: 0;
+        margin-bottom: 4em;
+        text-shadow: 0 1px 0 rgba(255,255,255,.5);
+        margin-left: -200px;
+			}
+.rounded-list a {
+      width:90px;
+			position: relative;
+			display: block;
+			padding: 0.4em 0.4em 0.4em 2em;
+			*padding: 0.4em;/*for ie6/7*/
+			margin: 0.5em 0;
+			background: #fff;
+			color: #444;
+			text-decoration: none;
+			/*CSS3属性*/
+			border-radius: 0.3em;/*制作圆角*/
+			/* transition动画效果*/
+			-moz-transition: all 0.3s ease-out;
+			-webkit-transition: all 0.3s ease-out;
+			-o-transition: all 0.3s ease-out;
+			-ms-transition: all 0.3s ease-out;
+			transition: all 0.3s ease-out;
+      
+		}
+		.rounded-list a:hover {
+			background: #eee;
+		}
+		.rounded-list a:hover::before {
+			/*悬停时旋转编码*/
+			-moz-transform: rotate(360deg);
+			-webkit-transform: rotate(360deg);
+			-o-transform: rotate(360deg);
+			-ms-transform: rotate(360deg);
+			transform: rotate(360deg);
+		}
+		.rounded-list a::before {
+			
+			content: counter(li);
+			counter-increment: li;
+			
+			position: absolute;
+			left: -1.3em;
+			top: 50%;
+			margin-top: -1.3em;
+			background: #8fbc8f;
+			height: 2em;
+			width: 2em;
+			line-height: 2em;
+			border: 0.3em solid #fff;
+			text-align: center;
+			font-weight: bold;
+			border-radius: 2em;
+			-webkit-transition: all 0.3s ease-out;
+			-moz-transition: all 0.3s ease-out;
+			-ms-transition: all 0.3s ease-out;
+			-o-transition: all 0.3s ease-out;
+			transition: all 0.3s ease-out;
+		}
 .el-space {
   display: block;
 }
-.aaaaa{
-    margin-left:-350px;
+.aaaaa {
+  margin-left: -350px;
 }
 .file-list-container {
   width: 300px;
   height: auto;
   overflow: auto;
-  margin-left:-11500px;
+  margin-left: -11500px;
   text-align: left;
 }
 
@@ -315,196 +371,214 @@ export default {
   overflow-x: auto; /* Add scroll bar if necessary */
 }
 
-::v-deep .el-descriptions-title
-{
-  text-align:center;
+::v-deep .el-descriptions-title {
+  text-align: center;
 }
-.center
-{
-  text-align:center;
-  margin-left:290px;
+.center {
+  text-align: center;
+  margin-left: 290px;
 }
 
-h4{
-  text-align:center
+h4 {
+  text-align: center;
 }
-h1
-{
-  text-align:center
+h1 {
+  text-align: center;
 }
 hr {
-    display: block;
-    height: 0.5px;
-    background: transparent;
-    width: 100%;
-    border: none;
-    border-top: solid 0.5px #f0f0f0;
+  display: block;
+  height: 0.5px;
+  background: transparent;
+  width: 100%;
+  border: none;
+  border-top: solid 0.5px #f0f0f0;
 }
-el-descriptions
-{
-  text-align:center
+el-descriptions {
+  text-align: center;
 }
-.select-button-container
-{
-  text-align:center
+.select-button-container {
+  text-align: center;
 }
 
-.load{
-  text-align:center
+.load {
+  text-align: center;
 }
-.el-upload__tip
-{
-  text-align:center
+.el-upload__tip {
+  text-align: center;
 }
-.file-list-container
-{
-  text-align:center
+.file-list-container {
+  text-align: center;
 }
-.description-container
-{
-  text-align:center
+.description-container {
+  text-align: center;
 }
-.el-descriptions_title
-{
-  text-align:center;
+.el-descriptions_title {
+  text-align: center;
 }
 
-.el-descriptions{
+.el-descriptions {
   width: 800px;
 }
 
 .el-input {
+  width: 220px;
+}
+.mybutton1 {
+  margin-left: -20px;
+  background-color: #8fbc8f;
+}
+.mybutton2 {
+  background-color: #8fbc8f;
+}
+.card .content {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+}
+.card h3 {
+  font-size: 1.8em;
+  z-index: 1;
+}
+.container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 1000px;
+  flex-wrap: wrap;
+  z-index: 1;
+  border: 1px solid #ccc;
+  background: #f9f9fb;
+  border-radius: 3%;
+  overflow: hidden;
+  margin: 0 auto;
+}
+.top {
+  height: 60px;
+  background: #419f60;
+  color: #fff;
+  font-size: 20px;
+  line-height: 60px;
+  padding: 0px 20px;
+  box-sizing: border-box;
+}
 
-      width: 220px;
-
-    }
-.mybutton{
-    margin-left:-20px;
+.container > div {
+  width: 100% !important;
 }
-.container{
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    max-width: 1200px;
-    flex-wrap: wrap;
-    z-index: 1;
+.container .card1 {
+  position: relative;
+  margin: 30px;
+  /* box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5); */
+  border-radius: 15px;
+  /* background: rgba(255, 255, 255, 0.1); */
+  overflow: hidden;
+  /* display: flex; */
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  border-left: 1px solid rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(5px);
+  margin-top:-5px;
 }
-.container .card1{
-    position: relative;
-    width: 480px;
-    height: 450px;
-    margin: 30px;
-    box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
-    border-radius: 15px;
-    background: rgba(255, 255, 255, 0.1);
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-top: 1px solid rgba(255, 255, 255, 0.5);
-    border-left: 1px solid rgba(255, 255, 255, 0.5);
-    backdrop-filter: blur(5px);
-
+.container .card1 .content {
+  padding: 20px;
+  text-align: center;
+  transform: translateY(100px);
+  opacity: 0;
+  transition: 0.5s;
 }
-.container .card1 .content{
-    padding: 20px;
-    text-align: center;
-    transform: translateY(100px);
-    opacity: 0;
-    transition: 0.5s;
+.container .card1 .content {
+  transform: translateY(0px);
+  opacity: 1;
 }
-.container .card1:hover .content{
-    transform: translateY(0px);
-    opacity: 1;
+.container .card1 .content h2 {
+  position: absolute;
+  top: -60px;
+  right: 1px;
+  font-size: 10em;
+  color: rgba(255, 255, 255, 0.05);
+  pointer-events: none;
 }
-.container .card1 .content h2{
-    position: absolute;
-    top: -60px;
-    right: 1px;
-    font-size: 10em;
-    color: rgba(255, 255, 255, 0.05);
-    pointer-events: none;
+.container .card1 .content h3 {
+  font-size: 1.8em;
+  color: #black;
+  z-index: 1;
 }
-.container .card1 .content h3{
-    font-size: 1.8em;
-    color: #black;
-    z-index: 1;
+.container .card1 .content p {
+  font-size: 1em;
+  color: #black;
+  font-weight: 300;
 }
-.container .card1 .content p{
-    font-size: 1em;
-    color: #black;
-    font-weight: 300;
+.container .card1 .content a {
+  position: relative;
+  display: inline-block;
+  padding: 8px 20px;
+  margin-top: 15px;
+  background: #black;
+  color: #000;
+  border-radius: 20px;
+  text-decoration: none;
+  font-weight: 500;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
 }
-.container .card1 .content a{
-    position: relative;
-    display: inline-block;
-    padding: 8px 20px;
-    margin-top: 15px;
-    background: #black;
-    color: #000;
-    border-radius: 20px;
-    text-decoration: none;
-    font-weight: 500;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+.container .card2 {
+  position: relative;
+  /* width: 1030px; */
+  /* height: 750px; */
+  margin: 30px;
+  /* box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5); */
+  border-radius: 15px;
+  background: rgba(255, 255, 255, 0.1);
+  overflow: hidden;
+  /* display: flex; */
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  border-left: 1px solid rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(5px);
+  margin-top:-65px;
 }
-.container .card2{
-    position: relative;
-    width: 1030px;
-    height: 750px;
-    margin: 30px;
-    box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
-    border-radius: 15px;
-    background: rgba(255, 255, 255, 0.1);
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-top: 1px solid rgba(255, 255, 255, 0.5);
-    border-left: 1px solid rgba(255, 255, 255, 0.5);
-    backdrop-filter: blur(5px);
-
+.container .card2 .content {
+  padding: 20px;
+  text-align: center;
+  transform: translateY(100px);
+  opacity: 0;
+  transition: 0.5s;
 }
-.container .card2 .content{
-    padding: 20px;
-    text-align: center;
-    transform: translateY(100px);
-    opacity: 0;
-    transition: 0.5s;
+.container .card2 .content {
+  transform: translateY(0px);
+  opacity: 1;
 }
-.container .card2:hover .content{
-    transform: translateY(0px);
-    opacity: 1;
+.container .card2 .content h2 {
+  position: absolute;
+  top: -60px;
+  right: 1px;
+  font-size: 10em;
+  color: rgba(255, 255, 255, 0.05);
+  pointer-events: none;
 }
-.container .card2 .content h2{
-    position: absolute;
-    top: -60px;
-    right: 1px;
-    font-size: 10em;
-    color: rgba(255, 255, 255, 0.05);
-    pointer-events: none;
+.container .card2 .content h3 {
+  font-size: 1.8em;
+  color: #black;
+  z-index: 1;
 }
-.container .card2 .content h3{
-    font-size: 1.8em;
-    color: #black;
-    z-index: 1;
+.container .card2 .content p {
+  font-size: 1em;
+  color: #black;
+  font-weight: 300;
 }
-.container .card2 .content p{
-    font-size: 1em;
-    color: #black;
-    font-weight: 300;
-}
-.container .card2 .content a{
-    position: relative;
-    display: inline-block;
-    padding: 8px 20px;
-    margin-top: 15px;
-    background: #black;
-    color: #000;
-    border-radius: 20px;
-    text-decoration: none;
-    font-weight: 500;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+.container .card2 .content a {
+  position: relative;
+  display: inline-block;
+  padding: 8px 20px;
+  margin-top: 15px;
+  background: #black;
+  color: #000;
+  border-radius: 20px;
+  text-decoration: none;
+  font-weight: 500;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
 }
 .el-space {
   display: block;
@@ -514,12 +588,11 @@ el-descriptions
   width: 300px;
   height: auto;
   overflow: auto;
-  margin-left:500px;
+  margin-left: 500px;
   text-align: left;
 }
-.file
-{
-  text-align:left;
+.file {
+  text-align: left;
 }
 .description-container {
   max-width: 100%;
@@ -531,77 +604,49 @@ el-descriptions
   overflow-x: auto; /* Add scroll bar if necessary */
 }
 
-::v-deep .el-descriptions-title
-{
-  text-align:center;
+::v-deep .el-descriptions-title {
+  text-align: center;
 }
-.center
-{
-  text-align:center;
-  margin-left:290px;
+.center {
+  text-align: center;
+  margin-left: 290px;
 }
 
-h4{
-  text-align:center
+h4 {
+  text-align: center;
 }
-h1
-{
-  text-align:center
+h1 {
+  text-align: center;
 }
 hr {
-    display: block;
-    height: 0.5px;
-    background: transparent;
-    width: 100%;
-    border: none;
-    border-top: solid 0.5px #f0f0f0;
+  display: block;
+  height: 0.5px;
+  background: transparent;
+  width: 100%;
+  border: none;
+  border-top: solid 0.5px #f0f0f0;
 }
-el-descriptions
-{
-  text-align:center
+el-descriptions {
+  text-align: center;
 }
-.select-button-container
-{
-  text-align:center
+.select-button-container {
+  text-align: center;
 }
 
-.load{
-  text-align:center
+.load {
+  text-align: center;
 }
-.el-upload__tip
-{
-  text-align:center
+.el-upload__tip {
+  text-align: center;
 }
-.file-list-container
-{
-  text-align:center
+.file-list-container {
+  text-align: center;
 }
-.description-container
-{
-  text-align:center
+.description-container {
+  text-align: center;
 }
-.el-descriptions_title
-{
-  text-align:center;
+.el-descriptions_title {
+  text-align: center;
 }
-.line {
-    border-right: 2px solid #000;
-    border-right: .15em solid green;
-    font-size: 200%; /* 字体大小 */
-    text-align: center; /* 文字横向居中 */
-    white-space: nowrap; /* 文字不换行 */
-    overflow: hidden; /* 多余的文字内容隐藏 */
-    animation: borders 0.1s infinite normal,
-    widths 2s steps(13); /* 重点 steps() */
-}
-   /* 边框闪烁动画 模拟指针闪烁 */
-@keyframes broders {
-    from {border-right-color:red;}
-    to {border-right-color: #fff;}
-}
-/* 容器宽的改变动画 */
-@keyframes widths {
-    from {width: 0;}
-    to {width: 40%;} /* 也可以是固定宽 */
-}
+
 </style>
